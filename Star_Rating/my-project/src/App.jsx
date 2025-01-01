bdc6ec1566cec41a5407249a9c87f135fd676bc3
vedantupadhye <vedupadhye10@gmail.com>
2025-01-01 11:31:40 +0530

    1-1-25

--- Star_Rating/my-project/src/App.jsx
+++ Star_Rating/my-project/src/App.jsx
@@ -1,7 +1,32 @@
-export default function App() {
+import {React,useState} from 'react';
+import './App.css';
+
+
+export default function App({starCount = 5}){
+
+  const [num,setNum] = useState();
+  const [hover ,setHover] = useState();
+  console.log(hover);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold text-center ">
+        Star Ratings
+        <div className='cursor-pointer'>
+          {
+            new Array(starCount).fill(0).map((value,index) =>{
+              return(
+                <span 
+               key={index} className={index < num  || index < hover ?"gold":""} 
+                 onClick={() =>setNum(index+1)}
+                 onMouseEnter={()=>setHover(index+1)}
+                 onMouseLeave={() =>setHover(0)}>&#9733;</span>
+              )
+            }
+          )
+          }
+        </div>
+      </h1>
+    </div>
   )
 }
