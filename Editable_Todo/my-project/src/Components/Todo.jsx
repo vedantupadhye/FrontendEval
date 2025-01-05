3a037c8533a14c5c818c34661bcc567df5cce3ba
vedantupadhye <vedupadhye10@gmail.com>
2025-01-05 23:49:50 +0530

    5-1-25

--- Editable_Todo/my-project/src/Components/Todo.jsx
+++ Editable_Todo/my-project/src/Components/Todo.jsx
@@ -1,49 +1,92 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Todo = () => {
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const [isEditing, setIsEditing] = useState(null); // Track the index of the item being edited
 
-    const [todo,setTodo]=useState('')
-    const [todoList,setTodoList]=useState([])
-   
-    const handleTodoList = ()=>{
-        setTodoList([...todoList,todo])      
+  // Add a new todo
+  const handleTodoList = () => {
+    if (todo.trim() !== '') {
+      setTodoList([...todoList, todo]);
+      setTodo(''); // Clear input field
     }
-    const handleDeleteTodo = (index) => {
-        const updatedTodoList = todoList.filter((_, i) => i !== index); // Remove the selected item
-        setTodoList(updatedTodoList);
-    };
-    const handleEdit = (e) =>{
-        const newValue = e.target.value;
-        setTodo([...todo,newValue])
+  };
+
+  // Delete a todo
+  const handleDeleteTodo = (index) => {
+    const updatedTodoList = todoList.filter((_, i) => i !== index);
+    setTodoList(updatedTodoList);
+  };
+
+  // Start editing
+  const startEdit = (index) => {
+    setIsEditing(index);
+    setTodo(todoList[index]); // Set the input field with the current value of the item
+  };
+
+  // Save the edited todo
+  const handleSaveEdit = () => {
+    if (todo.trim() !== '' && isEditing !== null) {
+      const updatedTodoList = todoList.map((item, index) =>
+        index === isEditing ? todo : item // Update only the edited item
+      );
+      setTodoList(updatedTodoList);
+      setIsEditing(null); // Exit editing mode
+      setTodo(''); // Clear input field
     }
- 
+  };
+
   return (
     <div>
-        <div className='mx-4 justify-center'>
-            <input placeholder='type your todo' type='text' className='border-2 border-black rounded-md my-4'
-                onChange={(e)=>setTodo(e.target.value)}
-            ></input>
-            <button className='bg-black text-white rounded-md p-2 mx-2' onClick={handleTodoList}>Add</button>
-            <div>
-               {
-                todoList.map((item,index) =>{
-                    return (
-                        <div className='flex my-3 ' key={index}>
-                            <div className=' text-xl flex justify-between mx-3'>
-                                <p className='mr-4'>{index +1}</p>
-                                {item}                                               
-                                <button className='bg-red-500 text-white rounded-md p-2 mx-4' onClick={() =>handleDeleteTodo(index)}>Delete</button>   
-                                <button className='bg-blue-400 text-white rounded-md p-2 mx-4' onClick={(e)=>handleEdit} >edit</button>                        
-                            </div>
-                        </div>
-                    )
-                })
-               }
+      <div className="mx-4 justify-center">
+        <input
+          placeholder="Type your todo"
+          type="text"
+          value={todo}
+          className="border-2 border-black rounded-md my-4 p-2"
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        {isEditing !== null ? (
+          <button
+            className="bg-green-500 text-white rounded-md p-2 mx-2"
+            onClick={handleSaveEdit}
+          >
+            Save
+          </button>
+        ) : (
+          <button
+            className="bg-black text-white rounded-md p-2 mx-2"
+            onClick={handleTodoList}
+          >
+            Add
+          </button>
+        )}
+        <div>
+          {todoList.map((item, index) => (
+            <div className="flex my-3" key={index}>
+              <div className="text-xl flex justify-between mx-3 items-center">
+                <p className="mr-4">{index + 1}</p>
+                <p className="flex-1">{item}</p>
+                <button
+                  className="bg-blue-400 text-white rounded-md p-2 mx-2"
+                  onClick={() => startEdit(index)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="bg-red-500 text-white rounded-md p-2 mx-2"
+                  onClick={() => handleDeleteTodo(index)}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
+          ))}
         </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
