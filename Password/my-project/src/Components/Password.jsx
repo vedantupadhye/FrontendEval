5fd4c723eedb28c52f4a217fd92c8292ff9e74ad
vedantupadhye <vedupadhye10@gmail.com>
2025-01-17 23:28:20 +0530

    17-1

---
+++ Password/my-project/src/Components/Password.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Password = () => {
+  return (
+    <div>Password
+        <div>
+            <input type="password" name="password" placeholder="Enter your password" />
+        </div>
+    </div>
+  )
+}
+
+export default Password
